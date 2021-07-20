ac1bed6a72bb794727fa5cf2d61f27aab734d4bf
OctaviusBowman <octaviusbowman@gmail.com>
2021-07-19 21:07:23 -0400

    Custom hooks, that retrieve browser page data from firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Home, Browse, Signin, Signup } from "./pages/index"
-import * as ROUTES from './constants/routes'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Home, Browse, Signin, Signup } from "./pages/index";
+import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks/index';
 
 function App() {
+  const { user } = useAuthListener();
+  console.log(user);
+
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
         <Signin />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
+      </IsUserRedirect>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact>
         <Signup />
-      </Route>
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+        <Browse />
+      </ProtectedRoute>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+        <Home />
+      </IsUserRedirect>
     </Router>
   )
 }
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useContent } from '../hooks/index';
 
-export default function Browse({ }) {
+export default function Browse() {
+
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+    console.log(series);
+    console.log(films);
+
     return (
         <p>Hello from the browse component!</p>
     )
